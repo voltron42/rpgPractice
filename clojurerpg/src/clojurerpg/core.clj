cc1a4b0110f642f8a6b4cf73eac4ba147a75909d
Daniel Johnson <daniel.johnson@surescripts.com>
2017-04-04 16:01:51 -0500

    fleshing out game

--- clojurerpg/src/clojurerpg/core.clj
+++ clojurerpg/src/clojurerpg/core.clj
@@ -20,7 +20,13 @@
             '* *
             '/ /
             '> >
+            'd4 (d 4)
+            'd6 (d 6)
+            'd8 (d 8)
+            'd10 (d 10)
+            'd12 (d 12)
             'd20 (d 20)
+            'd100 (d 100)
             }
      resolver
      (fn resolveExp [vars exp]
