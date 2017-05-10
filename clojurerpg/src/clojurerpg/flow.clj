96d79a20bf80e523ee7e4ce60f132a7388e05238
Daniel Johnson <daniel.johnson@surescripts.com>
2017-05-10 16:17:51 -0500

    adding a try-catch to run flow

--- clojurerpg/src/clojurerpg/flow.clj
+++ clojurerpg/src/clojurerpg/flow.clj
@@ -3,18 +3,13 @@
             [clojurerpg.common :as common])
   (:import (javafx.animation PauseTransition)))
 
-(def decodeExpression (
-                        (fn []
+(def decodeExpression ((fn []
                           (let
-                            [
-                             d (fn makeDice [sideCount]
+                            [d (fn makeDice [sideCount]
                                  (fn rollDice
                                    ([] (rollDice 1))
-                                   ([numberOf] (reduce + 0 (map #(inc (rand-int %)) (repeat numberOf sideCount))))
-                                   )
-                                 )
-                             opmap {
-                                    '+ +
+                                   ([numberOf] (reduce + 0 (map #(inc (rand-int %)) (repeat numberOf sideCount))))))
+                             opmap {'+ +
                                     '- -
                                     '* *
                                     '/ /
@@ -38,96 +33,54 @@
                                     '!?= #(if % false true)
                                     '$#!+ #(throw (Exception. (str %)))
                                     '| (fn [firstArg & args] (reduce #(or %1 %2) firstArg args))
-                                    '& (fn [firstArg & args] (reduce #(and %1 %2) firstArg args))
-                                    }
-                             ]
-                            (common/expressionDecoder opmap)
-                            )
-                          )
-                        )
-  )
+                                    '& (fn [firstArg & args] (reduce #(and %1 %2) firstArg args))}]
+                            (common/expressionDecoder opmap)))))
 
 (defn typemap [recursor]
-  (let [
-        commands {
-                  '... (fn [line state]
+  (let [commands {'... (fn [line state]
                            (do
                              (Thread/sleep 1000)
-                             state
-                             )
-                           )
-                  }
-        ]
-    {
-     vector? (fn [line state]
-               (let [
-                     expression (first line)
-                     sublines (rest line)
-                     ]
+                             state))}]
+    {vector? (fn [line state]
+               (let [expression (first line)
+                     sublines (rest line)]
                  (if ((decodeExpression expression) state)
                    (recursor sublines state)
-                   state
-                   )
-                 )
-               )
+                   state)))
      list? (fn [line state]
              (do
-               (println (str line " = "
-                             ((decodeExpression line) state)
-                             )
-                 )
-               state
-               )
-             )
+               (println (str line " = " ((decodeExpression line) state)))
+               state))
      map? (fn [line state]
             (reduce (fn [out [k v]]
                       (if (nil? v)
                         (dissoc out k)
-                        (assoc out k ((decodeExpression v) state))
-                        )
-                      ) state line)
-            )
+                        (assoc out k ((decodeExpression v) state))))
+                    state line))
      keyword? (fn [line state]
                 (do
                   (println (state line))
-                  state
-                  )
-                )
+                  state))
      symbol? (fn [line state]
                (if (contains? commands line)
                  ((commands line) line state)
-                 (throw (str line " is not a valid command"))
-                 )
-               )
-     }
-    )
-  )
+                 (throw (str line " is not a valid command"))))}))
 
 (defn runflow [flow flowstate]
-  (loop [lines flow state flowstate]
-    (let [line (first lines)
-          remaining (rest lines)
-          typeaction (first (map second (filter (fn [[k v]] (k line)) (typemap runflow))))
-          typefunc (if (nil? typeaction) (fn [l s] (do (println l)) s) typeaction)
-          newstate (typefunc line state)
-          ]
-      (if (empty? remaining)
-        newstate
-        (recur remaining newstate)
-        )
-      )
-    )
-  )
+  (try
+    (loop [lines flow state flowstate]
+      (let [line (first lines)
+            remaining (rest lines)
+            typeaction (first (map second (filter (fn [[k v]] (k line)) (typemap runflow))))
+            typefunc (if (nil? typeaction) (fn [l s] (do (println l)) s) typeaction)
+            newstate (typefunc line state)]
+        (if (empty? remaining)
+          newstate
+          (recur remaining newstate))))
+    (catch Throwable t
+      (println (.getMessage t)))))
 
 (defn -main [& args]
   (when (empty? args) (throw "must pass in a path to the program file"))
-  (let
-    [
-     filename (first args)
-     data (edn/read-string (slurp filename))
-     state {}
-     result (runflow data state)
-     ]
-    "COMPLETE"
-    )
-  )
+  (let [_ (runflow (edn/read-string (slurp (first args))) {})]
+    "COMPLETE"))
