0c205dc2e20a4f1ef0478f07138ba48c1e2c018b
Daniel Johnson <voltron42@users.noreply.github.com>
2024-07-10 21:53:44 +0000

    building out parsing logic

--- cypherflow/src/cypherflow/core.clj
+++ cypherflow/src/cypherflow/core.clj
@@ -1,11 +1,35 @@
 (ns cypherflow.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [clojure.string :as str])
   (:gen-class))
 
+(def replacements
+  [[#"\:" " :_ "]
+   [#"\;" " "]
+   [#"\*" " * "]
+   [#"\/" " / "]
+   [#"\>" " > "]
+   [#"\<" " < "]
+   [#"\=" " = "]
+   [#"\+" " + "]
+   [#"\-" " - "]
+   [#"  " " "]
+   [#"  " " "]
+   [#"< >" "<>"]
+   [#"< =" "<="]
+   [#"> =" ">="]])
+
 (defn -main [& _]
-  (with-open [rdr (io/reader "/resources/werewolves.cyf")]
-    (->> (line-seq rdr)
-         (map (fn [line] 
-                (read-string (str "[" line "]"))))
-         
-        )))
+  (with-open [rdr (io/reader "./resources/werewolves.cyf")]
+    (let [code (->> (line-seq rdr)
+                    (filter #(not-empty (str/trim %)))
+                    (map (fn [line]
+                           (let [replaced (reduce #(str/replace %1 (first %2) (second %2)) line replacements)
+                                 text (str/trim (str "[" replaced "]"))]
+                             (println text)
+                             (let [code-line (read-string text)]
+                               (pp/pprint code-line)
+                               code-line))))
+                    (vec))]
+      (println (count code)))))
