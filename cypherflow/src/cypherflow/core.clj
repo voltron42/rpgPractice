68694487d4044cff20cbea764f7a49a0b0757ced
Daniel Johnson <voltron42@users.noreply.github.com>
2024-07-12 18:19:31 +0000

    attempting to build out code modeller

--- cypherflow/src/cypherflow/core.clj
+++ cypherflow/src/cypherflow/core.clj
@@ -1,53 +1,18 @@
 (ns cypherflow.core
-  (:require [clojure.java.io :as io]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.pprint :as pp]
-            [clojure.set :as set]
             [cypherflow.parse :as cfp])
   (:gen-class))
 
-(def reserved
-  {:comparators '#{< > = <>}
-   :operators '#{+ - * /}
-   :conjunctions '#{AND OR}
-   :functions '#{RND INT A}
-   :command-words '#{THEN ELSE TO}})
-
 (defn -main [& _]
+  (with-open [rdr (io/reader "./resources/werewolves.edn")]
+    (let [code (edn/read rdr)]
+      )))
+      
+(defn parse-and-write-file []
   (with-open [rdr (io/reader "./resources/werewolves.cyf")
               w (io/writer "./resources/werewolves.edn" :encoding "UTF-8")]
-    (let [code (cfp/parse (line-seq rdr))
-          symbols (reduce 
-                   (fn [outList line]
-                     (reduce 
-                      #(if (symbol? %2) (conj %1 %2) %1)
-                      outList
-                      line))
-                   (sorted-set)
-                   code)
-          command-words (reduce
-                         #(if (or (> 3 (count %2)) (not= (nth %2 2) (symbol "=")))
-                            (do
-                              ;;(pp/pprint { :second (second %2) :line %2})
-                              (conj %1 (second %2)))
-                            %1)
-                         (sorted-set)
-                         code)
-          variables (reduce
-                         #(if (and (<= 3 (count %2)) (= (nth %2 2) (symbol "=")))
-                            (do
-                              ;;(pp/pprint { :second (second %2) :line %2})
-                              (conj %1 (second %2)))
-                            %1)
-                         (sorted-set)
-                         code)
-          known-reserved (set/union (apply set/union (vals reserved)) command-words variables)
-          unknown (set/difference symbols known-reserved)
-          all-symbols (assoc
-                       reserved
-                       :command-words (set/union (:command-words reserved) command-words)
-                       :variables variables
-                       :unknown unknown)]
+    (let [code (cfp/parse (line-seq rdr))]
       (println (count code))
-      (.write w (with-out-str (pp/pprint code)))
-      (pp/pprint all-symbols))))
-      
+      (.write w (with-out-str (pp/pprint code))))))
--- cypherflow/src/cypherflow/parse.clj
+++ cypherflow/src/cypherflow/parse.clj
@@ -1,7 +1,5 @@
 (ns cypherflow.parse
-  (:require
-   [clojure.pprint :as pp]
-   [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
 (def replacements
   [[#"\:" " :_ "]
@@ -25,10 +23,150 @@
   (->> lines
        (filter #(not-empty (str/trim %)))
        (map (fn [line]
-              (let [replaced (reduce #(str/replace %1 (first %2) (second %2)) line replacements)
-                    text (str/trim (str "[" replaced "]"))]
-                (println text)
-                (let [code-line (read-string text)]
-                  (pp/pprint code-line)
-                  code-line))))
+              (let [replaced (reduce #(str/replace %1 (first %2) (second %2)) line replacements)]
+                (read-string (str/trim (str "[" replaced "]"))))))
        (vec)))
+
+(defmulti model-commands
+  (fn [queue _]
+    (first queue)))
+
+(defn model [line]
+  (let [line-number (first line)
+        queue (vec (rest line))]
+    {:line-number line-number
+     :commands (model-commands queue [])}))
+
+(defn has-operator [coll operator]
+  (< -1 (.indexOf coll operator)))
+
+(defn partition-by-operators [operators coll]
+  (->> coll
+       (partition-by operators)
+       (filter #(or (< 1 (count %)) (not (operators (first %)))))))
+
+(defmulti model-function-call first)
+
+(defmulti model-operand type)
+
+; todo - defmethod model-operand java.lang.Long
+; todo - defmethod model-operand clojure.lang.Symbol
+
+(defn model-expression [expression]
+  (let [out (reduce
+             (fn [out operator]
+               (if out out
+                   {:expression :operation
+                    :operator operator
+                    :operands (->> expression
+                                   (partition-by-operators #{operator}))}))
+             false
+             '[+ - * /])]
+    (if out out
+        (if (= 2 (count expression))
+          (model-function-call expression)
+          (model-operand (first expression))))))
+
+(defmethod model-function-call 'RND [_]
+  {:expression :function
+   :function :random})
+
+; todo - model-function-call 'INT
+; todo - model-function-call :default
+
+(defn parse-comparison [predicate]
+  (let [[left operator right] (partition-by '#{< > = <> <= >=} predicate)]
+    {:predicate (first operator)
+     :left (model-expression left)
+     :right (model-expression right)}))
+
+(defn parse-conjuctions [predicate conjunction]
+  {:predicate conjunction
+   :comparisons (->> predicate
+                     (partition-by-operators #{conjunction})
+                     (map parse-comparison))})
+
+(defn model-predicate [predicate]
+  (if (has-operator predicate 'AND)
+    (parse-conjuctions predicate 'AND)
+    (if (has-operator predicate 'OR)
+      (parse-conjuctions predicate 'OR)
+      (parse-comparison predicate))))
+
+(defn split-queue [queue item]
+  [(take-while #(not= item %) queue)
+   (drop-while #(not= item %) queue)])
+
+(defmethod model-commands 'IF [queue out]
+  (let [[if-block then-queue] (split-queue queue 'THEN)
+        if-test (model-predicate if-block)
+        [then-block else-block] (split-queue then-queue 'ELSE)
+        then (model-commands then-block [])]
+    (if (empty? else-block)
+      (conj out {:command :if-then
+                 :if if-test
+                 :then then})
+      (conj out {:command :if-then-else
+                 :if if-test
+                 :then then
+                 :else (model-commands else-block [])}))))
+
+(defn step-commands [queue out func]
+  (let [[data next-queue] (split-queue (rest queue) :_)]
+    (model-commands
+     (rest next-queue)
+     (conj out (func data)))))
+
+(defmethod model-commands 'ASSIGN [queue out]
+  (step-commands queue out
+                 #(hash-map
+                   :command :assign
+                   :variable (first %)
+                   :expression (model-expression (drop 2 %)))))
+
+(defmethod model-commands 'CLS [queue out]
+  (model-commands (rest queue) (conj out {:command :cls})))
+
+(defmethod model-commands 'DATA [queue out]
+  (step-commands queue out #(hash-map :command :data :data %)))
+
+(defmethod model-commands 'DIM [queue out]
+  (step-commands queue out #(hash-map :command :read :variable (first %) :dimensions (vec (second %)))))
+
+(defmethod model-commands 'END [queue out]
+  (model-commands (rest queue) (conj out {:command :end})))
+
+(defmethod model-commands 'FOR [queue out]
+  (step-commands queue out #(hash-map :command :for :variable (first %) :start (nth % 2) :end (nth % 4))))
+
+(defmethod model-commands 'GOSUB [queue out]
+  (step-commands queue out #(hash-map :command :gosub :target (first %))))
+
+(defmethod model-commands 'GOTO [queue out]
+  (step-commands queue out #(hash-map :command :goto :target (first %))))
+
+(defmethod model-commands 'INPUT [queue out]
+  (step-commands queue out #(hash-map :command :input :variable (first %))))
+
+(defmethod model-commands 'NEXT [queue out]
+  (step-commands queue out #(hash-map :command :next :variable (first %))))
+
+(defmethod model-commands 'ON [queue out]
+  (step-commands queue out #(hash-map :command :on :variable (first %) :targets (drop 2 %))))
+
+(defmethod model-commands 'PRINT [queue out]
+  (step-commands queue out #(hash-map :command :print :args (vec %))))
+
+(defmethod model-commands 'READ [queue out]
+  (step-commands queue out #(hash-map :command :read :variable (first %) :args (vec (second %)))))
+
+(defmethod model-commands 'REM [queue out]
+  (step-commands queue out #(hash-map :command :rem :comment (first %))))
+
+(defmethod model-commands 'RETURN [queue out]
+  (model-commands (rest queue) (conj out {:command :return})))
+
+(defmethod model-commands :_ [queue out]
+  (model-commands (rest queue) out))
+
+(defmethod model-commands nil [_ out] out)
